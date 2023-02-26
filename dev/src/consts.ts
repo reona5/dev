57f24eafbbb26fc4e0aeb13ddd4d54aef11863a2
reona5 <shimada.reona@gmail.com>
2023-02-26 18:36:00 +0900

    fix: Replace info of site

--- dev/src/consts.ts
+++ dev/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'reona.dev'
+export const SITE_DESCRIPTION = 'reona.dev は Astro + Vercel によって作成されています。reona5 のプログラミングに関する学びや日々の出来事を発信するウェブサイトです。'
