a6a562bda551e20fc5cc0c6516feacca71725001
reona5 <shimada.reona@gmail.com>
2023-03-06 23:08:02 +0900

    chore: Install astro image package

--- dev/astro.config.mjs
+++ dev/astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+  site: 'https://reona.dev',
+  integrations: [mdx(), sitemap(), tailwind(), image()],
+})
