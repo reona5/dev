b04f936a919c4309b347abc374138b674357997c
reona5 <shimada.reona@gmail.com>
2023-03-10 07:30:10 +0900

    chore: Migrate @astrojs/image components
    refs: https://docs.astro.build/en/guides/assets/#convert-from-astrojsimage

--- dev/astro.config.mjs
+++ dev/astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
 
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   markdown: {
     shikiConfig: {
       theme: 'dark-plus',
@@ -12,5 +14,5 @@ export default defineConfig({
     },
   },
   site: 'https://reona.dev',
-  integrations: [mdx(), sitemap(), tailwind(), image()],
+  integrations: [mdx(), sitemap(), tailwind()],
 })
