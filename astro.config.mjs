28315ca6e96f2af71d1c7f8bb7021967caa19cf7
reona5 <shimada.reona@gmail.com>
2023-03-11 09:24:12 +0900

    chore: Add vercel integration with settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+  }),
   experimental: {
     assets: true,
   },
   markdown: {
     shikiConfig: {
-      theme: 'dark-plus',
+      theme: "dark-plus",
       wrap: true,
     },
   },
-  site: 'https://reona.dev',
+  site: "https://reona.dev",
   integrations: [mdx(), sitemap(), tailwind()],
-})
+});
