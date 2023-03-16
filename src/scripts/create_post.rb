91b7e4250256cb9ea1de10493d4591a1335625a2
reona5 <shimada.reona@gmail.com>
2023-03-16 23:30:34 +0900

    Generate draft post when execute yarn create:post

--- src/scripts/create_post.rb
+++ src/scripts/create_post.rb
@@ -11,7 +11,7 @@ puts "\n"
 
 now = Time.now
 pub_date = now.strftime("%Y-%m-%dT%H:%M:%S")
-file_name = "#{now.strftime("%Y-%m-%d-%H-%M-%S")}.md"
+file_name = "#{now.strftime("%Y-%m-%d-%H-%M-%S")}.mdx"
 file_path = "#{POSTS_DIR}#{file_name}"
 
 File.open(file_path, 'w') do |file|
@@ -19,6 +19,7 @@ File.open(file_path, 'w') do |file|
   file.write("title: '#{title}'\n")
   file.write("description: '#{description}'\n")
   file.write("pubDate: '#{pub_date}+09:00'\n")
+  file.write("draft: true\n")
   file.write("---\n")
 end
 
