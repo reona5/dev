36d9ea48fec6a2698434b6f63d91255578f532bc
reona5 <shimada.reona@gmail.com>
2023-05-10 21:50:58 +0900

    chore: Move to another repository to create by Next.js

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: "./src/assets",
-  output: "static",
-  adapter: vercel(),
+  site: "https://dev-by-astro.github.io",
   experimental: {
     assets: true,
   },
@@ -17,6 +15,5 @@ export default defineConfig({
       wrap: true,
     },
   },
-  site: "https://reona.dev",
   integrations: [mdx(), sitemap(), tailwind()],
 });
