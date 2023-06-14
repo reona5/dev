ef93da24bf4146e515882d3a7b622efed567d219
reona5 <shimada.reona@gmail.com>
2023-06-14 22:59:16 +0900

    feat: Add tag list for the blog posts

--- next-contentlayer/app/drafts/[...slug]/page.tsx
+++ next-contentlayer/app/drafts/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/app/components/header";
 import { Mdx } from "@/app/components/mdx-components";
+import { TagList } from "@/app/components/tag-list";
 import { allPosts } from "contentlayer/generated";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
@@ -14,7 +15,7 @@ const getPostFromParams = async (params: DraftProps["params"]) => {
   const slug = params?.slug?.join("/");
   const post = allPosts.find((post) => post.slugAsParams === slug);
 
-  if (!post || post.isPublished) return
+  if (!post || post.isPublished) return;
 
   return post;
 };
@@ -44,7 +45,7 @@ export const generateStaticParams = async (): Promise<
 
 const DraftPage = async ({ params }: DraftProps) => {
   const post = await getPostFromParams(params);
-  const isDevelopment = process.env.NODE_ENV === "development"
+  const isDevelopment = process.env.NODE_ENV === "development";
 
   if (!post || !isDevelopment) {
     notFound();
@@ -54,13 +55,9 @@ const DraftPage = async ({ params }: DraftProps) => {
     <>
       <Header ariaCurrent="Drafts" />
       <main className="m-auto max-w-4xl p-6">
-        <article className="prose max-w-none dark:prose-invert">
-          <h1 className="mb-2">{post.title}</h1>
-          {post.description && (
-            <p className="mt-0 text-xl text-slate-700 dark:text-slate-200">
-              {post.description}
-            </p>
-          )}
+        <article className="prose max-w-none break-words dark:prose-invert">
+          <h1 className="mb-10">{post.title}</h1>
+          <TagList tags={post.tags} />
           <hr className="my-4" />
           <Mdx code={post.body.code} />
         </article>
