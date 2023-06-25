f6f0ff53af24a29890014b5aafd9846fc31d1cdb
reona5 <shimada.reona@gmail.com>
2023-06-25 23:29:38 +0900

    fix: Add metadataBase for opengraph-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export const metadata = {
     apple: [{ url: "/apple-icon.png" }],
   },
   manifest: "/manifest.json",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'),
 };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
