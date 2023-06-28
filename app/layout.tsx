0b4f939e8fcb2340d61fd34733b70e9b08b9ba7e
reona5 <shimada.reona@gmail.com>
2023-06-28 21:21:02 +0900

    chore: Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,37 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
+const title = process.env.NEXT_PUBLIC_SITE_TITLE ?? "";
+const description = process.env.NEXT_PUBLIC_SITE_DESCRIPTION;
+const url = process.env.NEXT_PUBLIC_SITE_URL;
+const google = process.env.GOOGLE_SITE_VERIFICATION_KEY;
+
 export const metadata = {
+  title: {
+    default: title,
+    template: `%s - ${title}`,
+  },
+  description,
+  openGraph: {
+    title: title,
+    description,
+    url,
+    siteName: title,
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@reona_5",
+  },
+  verification: {
+    google,
+  },
+  alternates: {
+    canonical: url,
+  },
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "gray" },
@@ -23,9 +53,7 @@ export const metadata = {
     apple: [{ url: "/apple-icon.png" }],
   },
   manifest: "/manifest.json",
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"
-  ),
+  metadataBase: new URL(url ?? "http://localhost:3000"),
 };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
