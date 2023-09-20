80b5d549db6ccc83edb4f3f070fb89ee614b2430
reona5 <shimada.reona@gmail.com>
2023-09-20 23:11:58 +0900

    chore: pluginSearchDirs が削除されたため、設定から除外する
    refs: https://github.com/prettier/prettier/issues/12475#issuecomment-1626722913

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,3 @@
 module.exports = {
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
-  pluginSearchDirs: false, // refs: https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
 };
